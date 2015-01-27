f8b2435c70a8348ef0a8d9cdc6cb4d7f6f4cd04b
Ronald van der Kooij <ronald@sharpsolutions.nl>
2015-01-28 00:26:04 +0100

    Only the ctrl key is now needed to draw a straigh line

--- src/leaflet.draw.straightlines.js
+++ src/leaflet.draw.straightlines.js
@@ -3,16 +3,14 @@
     var orignalMouseMove = L.Draw.Polyline.prototype._onMouseMove;
     var originalMouseUp = L.Draw.Polyline.prototype._onMouseUp;
 
-    var currentMarker, currentLine, horizontalLine, verticalLine, lastPoint, map;
+    var currentMarker, straightline, lastPoint, map, clickedOnMarker;
 
     window.onkeydown = function(e){
-        horizontalLine = e.shiftKey;
-        verticalLine = e.ctrlKey;
+        straightline = e.ctrlKey;
     };
 
     window.onkeyup = function(e){
-        horizontalLine = false;
-        verticalLine = false;
+        straightline = false;
     };
 
     L.Draw.Polyline.prototype._onMouseMove = function(e){
@@ -20,49 +18,64 @@
         
         map = e.target;
 
-        if((horizontalLine || verticalLine) && currentMarker)
+        map.on('draw:drawstop', function (e) {
+            lastPoint = null;
+        });
+
+        if((straightline) && currentMarker)
         {
             var currentPoint = map.latLngToLayerPoint(currentMarker.getLatLng());
             
-            if(horizontalLine){
+            if(isHorizontal(e.latlng)){
                 currentMarker.setLatLng(L.latLng(lastPoint.lat, currentMarker.getLatLng().lng));
                 e.layerPoint.y = currentPoint.y;
             }
-
-            if(verticalLine){
+            else{
                 currentMarker.setLatLng(L.latLng(currentMarker.getLatLng().lat, lastPoint.lng));
                 e.layerPoint.x = currentPoint.x;
             }
-            
-            currentMarker.update();
+
             this._updateGuide(e.layerPoint);
+            currentMarker.update();
         }
     };
 
     L.Draw.Polyline.prototype._onMouseUp = function(e){
         originalMouseUp.call(this, e);
 
-        lastPoint = e.latlng;
-        currentLine = getLayerOfType(L.Polyline);
-
-        if(horizontalLine || verticalLine){
-             var latLngs = currentLine.getLatLngs();
-
+        var currentLine = getLayerOfType(L.Polyline);
+        
+        currentMarker = getLayerOfType(L.Marker);
+        
+        currentMarker.on("mousedown", function(){
+            clickedOnMarker = true;
+        });
+        
+        if(clickedOnMarker){
+            clickedOnMarker = false;
+            return;
+        }
+        
+        if(straightline){
+             
+            var latLngs = currentLine.getLatLngs();
+            
             if(lastPoint && latLngs.length){
                 
-                if(horizontalLine){
-                    latLngs[latLngs.length - 1].lat = currentMarker.getLatLng().lat;
-                    latLngs[latLngs.length - 2].lat = currentMarker.getLatLng().lat;
+                if(isHorizontal(e.latlng)){
+                    latLngs[latLngs.length - 1].lat = lastPoint.lat;
+                    latLngs[latLngs.length - 2].lat = lastPoint.lat;
                 }
-                else if(verticalLine){
-                    latLngs[latLngs.length - 1].lng = currentMarker.getLatLng().lng;
-                    latLngs[latLngs.length - 2].lng = currentMarker.getLatLng().lng;
+                else {
+                    latLngs[latLngs.length - 1].lng = lastPoint.lng;
+                    latLngs[latLngs.length - 2].lng = lastPoint.lng;   
                 }
                 currentLine.redraw();
-                
+            
             }
         }
-        currentMarker = getLayerOfType(L.Marker);
+        
+        lastPoint = currentMarker.getLatLng();
     };
 
     function getLayerOfType(type){
@@ -73,5 +86,12 @@
             }
         });
         return result;
+    }
+
+    function isHorizontal(mousePosition){
+        var latDiff = Math.abs(mousePosition.lat - lastPoint.lat);
+        var lngDiff = Math.abs(mousePosition.lng - lastPoint.lng);
+
+        return latDiff < lngDiff;
     }
 })();
