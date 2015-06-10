ab68cc527ee566457b694f5f19ecba77a3f7d8f9
Maurice <maurice.de.beijer@gmail.com>
2015-06-10 14:48:13 +0200

    Specify optional start function

--- example/js/index.js
+++ example/js/index.js
@@ -1,11 +1,11 @@
 var map = L.map('map').setView([51.505, -0.09], 13);
 
 L.tileLayer('https://{s}.tiles.mapbox.com/v3/{id}/{z}/{x}/{y}.png', {
-	maxZoom: 18,
-	attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
-		'<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-		'Imagery © <a href="http://mapbox.com">Mapbox</a>',
-	id: 'examples.map-i875mjb7'
+    maxZoom: 18,
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+    '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+    'Imagery © <a href="http://mapbox.com">Mapbox</a>',
+    id: 'examples.map-i875mjb7'
 }).addTo(map);
 
 var drawnItems = new L.FeatureGroup();
@@ -19,7 +19,11 @@ var drawControl = new L.Control.Draw({
 });
 
 map.addControl(drawControl);
-map.initStraightLines();
+map.initStraightLines({
+    startFn: function (e) {
+        return e.shiftKey;
+    }
+});
 
 map.on('draw:created', function (e) {
     map.addLayer(e.layer);
