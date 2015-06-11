9db18b873f08f09069682c87de815478a4c97415
Maurice de Beijer <mauricedb@computer.org>
2015-06-11 10:15:26 +0200

    Don't leak global variables

--- example/js/index.js
+++ example/js/index.js
@@ -1,26 +1,29 @@
-var map = L.map('map').setView([51.505, -0.09], 13);
+(function () {
+    'use strict';
+    var map = L.map('map').setView([51.505, -0.09], 13);
 
-L.tileLayer('https://{s}.tiles.mapbox.com/v3/{id}/{z}/{x}/{y}.png', {
-	maxZoom: 18,
-	attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
-		'<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-		'Imagery © <a href="http://mapbox.com">Mapbox</a>',
-	id: 'examples.map-i875mjb7'
-}).addTo(map);
+    L.tileLayer('https://{s}.tiles.mapbox.com/v3/{id}/{z}/{x}/{y}.png', {
+        maxZoom: 18,
+        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+        '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+        'Imagery © <a href="http://mapbox.com">Mapbox</a>',
+        id: 'examples.map-i875mjb7'
+    }).addTo(map);
 
-var drawnItems = new L.FeatureGroup();
-map.addLayer(drawnItems);
+    var drawnItems = new L.FeatureGroup();
+    map.addLayer(drawnItems);
 
-var drawControl = new L.Control.Draw({
-    edit: {
-        featureGroup: drawnItems,
-        edit: true
-    }
-});
+    var drawControl = new L.Control.Draw({
+        edit: {
+            featureGroup: drawnItems,
+            edit: true
+        }
+    });
 
-map.addControl(drawControl);
-map.initStraightLines();
+    map.addControl(drawControl);
+    map.initStraightLines();
 
-map.on('draw:created', function (e) {
-    map.addLayer(e.layer);
-});
+    map.on('draw:created', function (e) {
+        map.addLayer(e.layer);
+    });
+}());
